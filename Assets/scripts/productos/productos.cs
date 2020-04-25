7d3f722baab7c8f76d2bb3fa6769837e71af444b
PepeLegend <30629957+PepeLegend@users.noreply.github.com>
2020-04-25 20:32:23 +0200

    Bases del proyecto establecidas
    - añadida clase producto
    - añadidas derivaciones de la clase producto.
    - añadido inventario manager
    - añadido  game manager

---
+++ Assets/scripts/productos/productos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public abstract class productos : ScriptableObject
+{
+    /*
+     * productos:
+     * clase padre de todos los productos que se crearan
+     * 
+     * parametros:
+     *  nombre (string): nombre del producto
+     *  descripcion (string): descripcion del producto
+     *  precio (string): precio del producto
+     */
+
+    //nombre del producto
+    public string nombre;
+
+    //descripcion del producto
+    public string descripcion;
+    
+    //precio del producto
+    public string precio;
+
+
+}
